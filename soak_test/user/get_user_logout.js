86a5690ae8c9d8c0d75c4cb68c4be14cd74d063c
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-09-27 15:17:52 -0300

    Add metrics tracking to various soak and stress test scripts
    - Introduced Counter for tracking the number of calls made in each script.
    - Added Gauge to measure blocked time during requests.
    - Implemented Rate to monitor the percentage of successful responses (status 200).
    - Included Trend to analyze waiting time for requests.
    - Updated all relevant test scripts in the soak_test and stress_spike_test directories, as well as user and store endpoints, to incorporate these metrics.

--- soak_test/user/get_user_logout.js
+++ soak_test/user/get_user_logout.js
@@ -1,5 +1,14 @@
 import http from 'k6/http';
 import {sleep, check} from 'k6';
+import { Counter} from 'k6/metrics';
+import { Gauge } from 'k6/metrics';
+import { Rate } from 'k6/metrics';
+import { Trend } from 'k6/metrics'; 
+
+const chamadas = new Counter('quantidade de chamadas');
+const myGauge = new Gauge('Tempo bloqueado');
+const myRate = new Rate('taxa req 200');
+const myTrend = new Trend('taxa de espera');
 
 export const options = {
     stages: [
@@ -16,4 +25,12 @@ export default function(){
     check(res, {
         'status code é 200': (r) => r.status === 200
     });
+    //contador
+    chamadas.add(1);
+    //medidor
+    myGauge.add(req.timings.blocked);
+    //taxa
+    myRate.add(req.status === 200);
+    //tendencia
+    myTrend.add(req.timings.waiting);
 }
