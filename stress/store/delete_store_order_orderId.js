0a386867614e166b2f8825a642fa2bf7a2a6cd49
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-30 12:31:03 -0300

    Atualizando arquivos de teste.

--- stress/store/delete_store_order_orderId.js
+++ stress/store/delete_store_order_orderId.js
@@ -1,5 +1,30 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { sleep } from 'k6';
+import { Counter, Trend } from 'k6/metrics'; //custom metric
+    http_req_duration:['p(95)<100'],
+stages: [
+        {
+            duration: '5m',
+            target: 100
+        },
+        {
+            duration: '30m',
+            target: 100
+        },
+        {
+            duration: '5m',
+            target: 0
+        }
+    ],
+    thresholds: {
+    http_req_duration:['p(95)<100'],
+    http_req_duration:['max<200'],
+    http_req_failed: ['rate<0.1'], //tava de falha, nes caso tem que ser inferior a 0.01 das solicitações
+    http_reqs: ['count>20'], //quantas solicitações quero, neste caso não pode ser menos de 20
+    http_reqs: ['rate>4'], //quantas solicitações por segundo, neste caso não pode ser menos de 4 por segundo
+    vus: ['value>9'], //o mínimo de usuários usados ao mesmo tempo
+    checks: ['rate>=0.98']
+  }
 
 export const options = {
     stages: [
